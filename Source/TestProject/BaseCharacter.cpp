6be062a50f650520e6ca0b615845bfc82f4a1b7c
captiosus <royxu652@yahoo.com>
2017-02-14 23:06:39 -0500

    init

---
+++ Source/TestProject/BaseCharacter.cpp
@@ -0,0 +1,59 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TestProject.h"
+#include "BaseCharacter.h"
+
+
+// Sets default values
+ABaseCharacter::ABaseCharacter()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ABaseCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ABaseCharacter::Tick( float DeltaTime )
+{
+	Super::Tick( DeltaTime );
+
+}
+
+// Called to bind functionality to input
+void ABaseCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
+void ABaseCharacter::CalculateHealth(float delta) {
+    health += delta;
+    CalculateDead();
+}
+
+
+void ABaseCharacter::CalculateDead() {
+    if (health <= 0) {
+        isDead = false;
+    } else {
+        isDead = true;
+    }
+}
+
+#if WITH_EDITOR
+void ABaseCharacter::PostEditChangeProperty(FPropertyChangedEvent& PropertyChangedEvent) {
+    isDead = false;
+    health = 100;
+    
+    Super::PostEditChangeProperty(PropertyChangedEvent);
+    
+    CalculateDead();
+}
+#endif
